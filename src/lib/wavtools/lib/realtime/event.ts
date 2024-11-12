396a985e4480a144b20ec9269f3504abe9e1d208
swan_escape <swan@escape.tech>
2024-11-12 22:35:43 +0100

    feat(lib/wavtools): extend event interfaces

--- src/lib/wavtools/lib/realtime/event.ts
+++ src/lib/wavtools/lib/realtime/event.ts
@@ -7,3 +7,19 @@ export interface RealtimeEvent {
   count?: number;
   event: { [key: string]: any };
 }
+
+interface ToolCallbackInput {
+  key: string;
+  value: string;
+}
+
+interface ToolCallbackOutput {
+  ok: boolean;
+}
+
+type ToolCallback = (input: ToolCallbackInput) => Promise<ToolCallbackOutput>;
+
+export interface ToolsT {
+  config: any;
+  callback: ToolCallback;
+}
