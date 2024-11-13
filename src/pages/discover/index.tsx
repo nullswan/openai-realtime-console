75d07f5337dd9855e8f6a185bc11cfbfc6f706de
Jean-Emmanuel Perramant <jperrama@gmail.com>
2024-11-13 11:41:02 +0100

    added the discover pages

--- src/pages/discover/index.tsx
+++ src/pages/discover/index.tsx
@@ -1,15 +1,20 @@
-import { useParams } from "react-router-dom";
-import Header from "../../components/Header/Header";
+import { useParams } from 'react-router-dom';
+import Header from '../../components/Header/Header';
+import { RoadmapResponse } from '../profile';
 
 export default function Discover() {
+  interface RoadmapResponse {
+    subject: string;
+    topics: string[];
+    description: string[]
+    image: string[]
+  }
   const { subjectId } = useParams<{ subjectId: string }>();
-
+  const subjects: Map<string, RoadmapResponse> = new Map();
   return (
     <div className="min-h-screen bg-white ">
       <Header />
-      <div>
-        {subjectId}
-      </div>
+      <div>{subjectId}</div>
     </div>
-  )
+  );
 }
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,10 +1,4 @@
 import React, { useState } from 'react';
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from '../../@/components/ui/accordion';
 import ProjectLearned from './ProjectLearned';
 
 export interface KeyConcepts {
